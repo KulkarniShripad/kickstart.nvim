465f0499fb89dda9859ac6b5bc6e73c62b0d9883
Shripad <Shripad>
2026-03-10 01:35:37 +0530

    Modified kickstart

--- lua/kickstart/health.lua
+++ lua/kickstart/health.lua
@@ -6,47 +6,47 @@
 --]]
 
 local check_version = function()
-  local verstr = tostring(vim.version())
-  if not vim.version.ge then
-    vim.health.error(string.format("Neovim out of date: '%s'. Upgrade to latest stable or nightly", verstr))
-    return
-  end
+    local verstr = tostring(v.version())
+    if not v.version.ge then
+        v.health.error(string.format("Neovim out of date: '%s'. Upgrade to latest stable or nightly", verstr))
+        return
+    end
 
-  if vim.version.ge(vim.version(), '0.11') then
-    vim.health.ok(string.format("Neovim version is: '%s'", verstr))
-  else
-    vim.health.error(string.format("Neovim out of date: '%s'. Upgrade to latest stable or nightly", verstr))
-  end
+    if v.version.ge(v.version(), '0.11') then
+        v.health.ok(string.format("Neovim version is: '%s'", verstr))
+    else
+        v.health.error(string.format("Neovim out of date: '%s'. Upgrade to latest stable or nightly", verstr))
+    end
 end
 
 local check_external_reqs = function()
-  -- Basic utils: `git`, `make`, `unzip`
-  for _, exe in ipairs { 'git', 'make', 'unzip', 'rg' } do
-    local is_executable = vim.fn.executable(exe) == 1
-    if is_executable then
-      vim.health.ok(string.format("Found executable: '%s'", exe))
-    else
-      vim.health.warn(string.format("Could not find executable: '%s'", exe))
+    -- Basic utils: `git`, `make`, `unzip`
+    for _, exe in ipairs { 'git', 'make', 'unzip', 'rg' } do
+        local is_executable = v.fn.executable(exe) == 1
+        if is_executable then
+            v.health.ok(string.format("Found executable: '%s'", exe))
+        else
+            v.health.warn(string.format("Could not find executable: '%s'", exe))
+        end
     end
-  end
 
-  return true
+    return true
 end
 
 return {
-  check = function()
-    vim.health.start 'kickstart.nvim'
+    check = function()
+        v.health.start 'kickstart.nvim'
 
-    vim.health.info [[NOTE: Not every warning is a 'must-fix' in `:checkhealth`
+        v.health.info [[NOTE: Not every warning is a 'must-fix' in `:checkhealth`
 
   Fix only warnings for plugins and languages you intend to use.
     Mason will give warnings for languages that are not installed.
     You do not need to install, unless you want to use those languages!]]
 
-    local uv = vim.uv or vim.loop
-    vim.health.info('System Information: ' .. vim.inspect(uv.os_uname()))
+        local uv = v.uv or v.loop
+        v.health.info('System Information: ' .. v.inspect(uv.os_uname()))
 
-    check_version()
-    check_external_reqs()
-  end,
+        check_version()
+        check_external_reqs()
+    end,
 }
